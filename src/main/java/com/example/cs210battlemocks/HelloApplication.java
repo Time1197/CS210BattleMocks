f9b361ee5ee8985bfa912de676f5d6cd8effd3c1
Keenan DePaz <kdepaz@san.rr.com>
2025-11-17 15:43:10 -0800

    Edited so window says "BattleMock1v1" instead of "Hello!"

--- src/main/java/com/example/cs210battlemocks/HelloApplication.java
+++ src/main/java/com/example/cs210battlemocks/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("battle-mock1v1.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("BattleMock1v1");
         stage.setScene(scene);
         stage.show();
     }
