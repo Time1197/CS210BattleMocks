91629ef6aeae1ad25148dcc1b4585823971c7996
Keenan DePaz <kdepaz@san.rr.com>
2025-11-17 16:59:54 -0800

    Added a 2v3, 2v4

--- src/main/java/com/example/cs210battlemocks/HelloApplication.java
+++ src/main/java/com/example/cs210battlemocks/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("battle-mock2v4.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("BattleMock1v1");
+        stage.setTitle("BattleMock2v4");
         stage.setScene(scene);
         stage.show();
     }
